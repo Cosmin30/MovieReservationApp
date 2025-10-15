7de05293d345235f961e3766121808056b7083c3
Cosmin30 <cocea2003@gmail.com>
2025-10-15 18:23:01 +0300

    first commit

---
+++ src/test/java/com/example/MovieReservationApp/MovieReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieReservationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
