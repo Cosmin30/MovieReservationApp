f1dbf25296934efbbec5a80f83499acf67686508
Cosmin30 <cocea2003@gmail.com>
2025-10-27 22:38:14 +0200

    entity user and reservation

---
+++ src/main/java/com/example/MovieReservationApp/domain/model/reservation/Reservation.java
@@ -0,0 +1,44 @@
+package com.example.MovieReservationApp.domain.model.reservation;
+
+import com.example.MovieReservationApp.domain.model.user.*;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "screening_id", nullable = false)
+    private Screening screening;
+
+    private OffsetDateTime createdAt;
+
+    private String status;
+
+    private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ticket> tickets;
+
+    @OneToMany(mappedBy = "reservation", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Payment> payments;
+}
+
+
---
+++ src/main/java/com/example/MovieReservationApp/domain/model/user/User.java
@@ -0,0 +1,34 @@
+package com.example.MovieReservationApp.domain.model.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private OffsetDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reservation> reservations;
+}
+
