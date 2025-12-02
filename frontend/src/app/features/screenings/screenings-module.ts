7997bff19fd9e8d92bd254c310f2558a0313dbe7
Cosmin30 <cocea2003@gmail.com>
2025-12-02 13:38:34 +0200

    creat structura aplicatie

---
+++ frontend/src/app/features/screenings/screenings-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ScreeningsRoutingModule } from './screenings-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ScreeningsRoutingModule
+  ]
+})
+export class ScreeningsModule { }
